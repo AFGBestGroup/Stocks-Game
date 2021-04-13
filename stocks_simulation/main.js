2b5ce4d2ec617f19579c897ab3ff02edf2424560
Jay Nagpaul <jaynagpaul@gmail.com>
2021-04-13 13:16:41 -0400

    Switch to Vue.js for updating HTML

--- stocks_simulation/main.js
+++ stocks_simulation/main.js
@@ -9,84 +9,53 @@ const TIME_JUMP = 5 * 24 * 60 * 60;
 
 const crashRate = 20;
 
-let stocks = [new Stock('Pear', 100, 1), new Stock('Doodle', 150, 1.25), new Stock('Mass Academy', 125, 0.8)];
-let bank = new Bank(DEFAULT_MONEY);
-
-function updatePage() {
-  let contentToUpdate = {
-    '#balance': () => bank.balance,
-  };
-  document.querySelector('#stocks').innerHTML="";
-  document.querySelector('#stocks').appendChild(buildHTML());
-  for (var key in contentToUpdate) {
-    if (document.querySelector(key))
-      document.querySelector(key).innerHTML = contentToUpdate[key]();
-  }
-}
-
-function buildHTML(){
-  var div = document.createElement("div");
-
-  for (var stock of stocks) {
-    var labelElement = document.createElement("p"); // <p> </p>
-    var label = document.createTextNode(stock.name); // label
-    labelElement.appendChild(label); // <p> label </p
-
-    var costElement = document.createElement("p"); // <p> </p>
-    var cost = document.createTextNode(stock.cost); // cost
-    costElement.appendChild(cost); // <p> cost </p>
-
-    var amountElement = document.createElement("p"); // <p> </p>
-    var amount = document.createTextNode(stock.shares); // amount
-    amountElement.appendChild(amount); // <p> amount </p>
-
-    div.appendChild(labelElement);
-    div.appendChild(costElement);
-    div.appendChild(amountElement);
-
-  }
-  return div;
-
-}
-
+let stocks = [
+  new Stock('Pear', 100, 1),
+  new Stock('Doodle', 150, 1.25),
+  new Stock('Mass Academy', 125, 0.8),
+];
 
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//Happens at constant rate, for now
 function isCrashed() {
   return Math.floor(Math.random() * crashRate) == 1;
 }
 
-setInterval(updatePage, 100);
+const App = {
+  data() {
+    return {
+      bank: new Bank(DEFAULT_MONEY),
+      stocks: stocks,
+    };
+  },
 
-function updateStockPrice() {
-  for(var s of stocks){
-    if (s.cost<=30*s.growthRate)
-      s.cost += rand(0,8*s.growthRate);
-    else if (isCrashed())
-      s.cost += rand(-30*s.growthRate, -15*s.growthRate);
-    else
-      s.cost += rand(-5*s.growthRate, 8*s.growthRate);
-  }
+  methods: {
+    buy(amount, index) {
+      this.stocks[index].buy(amount, this.bank);
+    },
+    sell(amount, index) {
+      this.stocks[index].sell(amount, this.bank);
+    },
 
-}
-
-setInterval(updateStockPrice, 700);
-
-window.buy = function buy(amount, index) {
-  stocks[index].buy(amount, bank);
+    sellAll(index) {
+      this.stocks[index].sellAll(this.bank);
+    },
+    buyAll(index) {
+      this.stocks[index].buyAll(this.bank);
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      for (var s of this.stocks) {
+        if (s.cost <= 30 * s.growthRate) s.cost += rand(0, 8 * s.growthRate);
+        else if (isCrashed())
+          s.cost += rand(-30 * s.growthRate, -15 * s.growthRate);
+        else s.cost += rand(-5 * s.growthRate, 8 * s.growthRate);
+      }
+    }, 700);
+  },
 };
 
-window.sell = function sell(amount, index) {
-  stocks[index].sell(amount, bank);
-};
-
-window.sellAll = function sellAll(index) {
-  stocks[index].sellAll(bank);
-};
-
-window.buyAll = function buyAll(index) {
-  stocks[index].buyAll(bank);
-};
+Vue.createApp(App).mount('#app');
