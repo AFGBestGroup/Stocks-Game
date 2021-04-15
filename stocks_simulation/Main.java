964afcec698575f948742b75b16ee38ce62592f5
IGuy37 <irgrzembski@wpi.edu>
2021-04-15 09:54:38 -0400

    Adds historical data to a JavaScript Array

--- stocks_simulation/Main.java
+++ stocks_simulation/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        String vals[] = ReadDataFromFile.load("stocks_data/AAPL.csv");
+        String vals[][] = ReadDataFromFile.load("../stocks_data/AAPL.csv");
 
         String js = "let AAPL = [";
-        for (String v : vals) {
-            js += v + ", ";
+        for (String v[] : vals) {
+            js += v[1] + ", ";
         }
         js += "]";
 
